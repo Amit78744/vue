350bf5562d7f27eda9bccf5038846218a348699c
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-16 15:57:12 +0530

    All Legal policy of website api completed.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,7 +15,7 @@
     
         <h1 v-if="currentUrl == 'http://abc.anviya.in/#/'">ABC Home Page</h1>
         <h1 v-else-if="currentUrl == 'http://xyz.anviya.in/#/'">XYZ Home Page</h1>
-        <h1 v-else>default Home Page</h1>
+        <h1 v-else>default Homes Page</h1>
         
         <!-- <h3 v-once>{{name}}</h3>
         <h3>{{a + b}}</h3>
