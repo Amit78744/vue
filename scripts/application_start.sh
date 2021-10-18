459f70280e2d18189628ea4db9872f7dfd282e77
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-18 18:42:07 +0530

    Rate and Reviews api completed.

---
+++ scripts/application_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#give permission for everything in the express-app directory
+sudo chown -R root /var/www/html
+
+#navigate into our working directory where we have all our github files
+cd /var/www/html/vue
