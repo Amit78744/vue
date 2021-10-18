2ccea0b4ebeadb9208b1b6b6cbb13f38ba496ead
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-18 08:50:34 +0530

    Rate and Reviews api completed.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,7 +15,7 @@
     
         <h1 v-if="currentUrl == 'http://abc.anviya.in/#/'">ABC Home Page</h1>
         <h1 v-else-if="currentUrl == 'http://xyz.anviya.in/#/'">XYZ Home Page</h1>
-        <h1 v-else>{{list}}default Homes Page</h1>
+        <h1 v-else>{{list[0].employee_name}} default Homes Page</h1>
         
         <!-- <h3 v-once>{{name}}</h3>
         <h3>{{a + b}}</h3>
@@ -75,7 +75,7 @@ export default {
         }
     },mounted()
     {
-        Vue.axios.post('http://52.77.235.8/getHomescreenData',{
+        Vue.axios.get('https://dummy.restapiexample.com/api/v1/employees',{
             //https://dummy.restapiexample.com/api/v1/employees
             website_name:'xyz'
         })
