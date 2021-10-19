37623736bf88668adb0ae944c1c988e4ababdb4c
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-19 06:06:42 +0530

    getStore by id response changes done.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chown -R root /var/www/html
+#give permission for everything in the vue.js directory
+sudo chown -R ec2-user /var/www/html
 
 #navigate into our working directory where we have all our github files
 cd /var/www/html/vue
+
+#install node modules
+npm install
+
+#start to build our vue.js app in the background
+npm run build > app.out.log 2> app.err.log < /dev/null &
