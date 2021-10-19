73f78c001b122057f768d44b39861e9cb4dbf6d8
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-19 06:14:23 +0530

    getStore by id response changes done.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,11 @@ sudo chown -R ec2-user /var/www/html
 #navigate into our working directory where we have all our github files
 cd /var/www/html/vue
 
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 #install node modules
 npm install
 npm run build
