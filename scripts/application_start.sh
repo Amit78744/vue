56c932b74bf85faf6b36d9baa509b1590f6194ea
ankitpansuria <ankit.pansuria@anviya.in>
2022-02-07 19:39:38 +0530

    set webhook url for cardup payments.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source /home/ec2-user/.bash_profile
 #give permission for everything in the vue.js directory
 sudo chown -R ec2-user /var/www/html
 
@@ -7,10 +7,8 @@ sudo chown -R ec2-user /var/www/html
 cd /var/www/html/vue
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+nvm install 14.4.0
 npm install
 npm run build
